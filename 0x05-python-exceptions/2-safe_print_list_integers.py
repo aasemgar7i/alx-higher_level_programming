78c80a66889ef87e6615c98d63ea2c947c52375f
aasemgar7i <aasemgar7i@gamil.com>
2023-12-18 15:41:02 +0200

    task2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    time = 0
+    for i in range(x):
+        try:
+            forp = my_list[i]
+            print("{:d}".format(forp), end="")
+            time += 1
+        except (ValueError, TypeError):
+            pass
+        except IndexError:
+            break
+    print()
+    return time

7a806d48d30e038e520421533e7d582e2d00471f
aasemgar7i <aasemgar7i@gamil.com>
2023-12-18 15:18:51 +0200

    hotfix0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list[], x=0):
     time = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
             time += 1
-        except ie:
+        except IndexError:
             break
     print()
-    return time
+    return (time)

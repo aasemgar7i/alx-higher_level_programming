4378049a57337758acd1434b97fe499553442ef1
aasemgar7i <aasemgar7i@gamil.com>
2023-12-18 15:41:45 +0200

    hotfix2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     time = 0
-    for i in range(x):
+    for i in range(0, x):
         try:
             forp = my_list[i]
             print("{:d}".format(forp), end="")

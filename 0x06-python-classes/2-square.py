c18babd9b93ced9fb06865b5ad7bf219928f524c
aasemgar7i <aasemgar7i@gamil.com>
2023-12-20 19:18:07 +0200

    hotfix

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,7 +11,7 @@ class Square:
                size (int): SIZE of input
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an intger")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         else:

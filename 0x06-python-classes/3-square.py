aaf56d9d676bf0c5aba5e254fb2777c8491ca6a4
aasemgar7i <aasemgar7i@gamil.com>
2023-12-20 19:22:16 +0200

    square3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""start here>>"""
+
+
+class Square:
+    """NEW Square!."""
+
+    def __init__(self, size=0):
+        """Args:
+               size (int): SIZE of input
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """back area of square size"""
+        return self.__size ** 2

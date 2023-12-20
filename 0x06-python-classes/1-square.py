a22696d27e054a54626fe00d6e79df6ceda51e0c
aasemgar7i <aasemgar7i@gamil.com>
2023-12-20 02:44:55 +0200

    return size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""start here>"""
+
+
+class Square:
+    """get size of input"""
+
+    def __init__(self, size):
+        """Args:
+               size (int): SIZE of input
+        """
+        self.__size = size

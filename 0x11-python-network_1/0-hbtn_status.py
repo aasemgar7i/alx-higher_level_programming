a8e2fa3dd6059416d339c57fe1c5a4dc6d349056
aasemgar7i <aasemgar7i@gamil.com>
2024-03-29 00:07:17 +0200

    updataing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,8 +11,11 @@ url = 'https://alx-intranet.hbtn.io/status'
 req = urllib.request.Request(url)
 
 with urllib.request.urlopen(req) as response:
-    body = response.read().decode('utf-8')
+    body = response.read()
+    utf8_content = body.decode('utf-8')
 
-print("- Body response:")
+print("Body response:")
 print("\t- type: {}".format(type(body)))
 print("\t- content: {}".format(body))
+print("\t- utf8 content: {}".format(utf8_content))
+

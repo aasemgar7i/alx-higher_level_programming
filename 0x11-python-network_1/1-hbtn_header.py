01231dadb9c74d8b30cca50d933c26f7f0430842
aasemgar7i <aasemgar7i@gamil.com>
2024-03-29 01:01:05 +0200

    hotfix fot task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -9,10 +9,10 @@
 import urllib.request
 import sys
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-req = urllib.request.Request(url)
-
-with urllib.request.urlopen(req) as response:
-    request_id = response.headers.get('X-Request-Id')
-    print(request_id)
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        x_request_id = response.getheader('X-Request-Id')
+        print(x_request_id)

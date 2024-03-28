8e88753c895a65c551aad539e9fe44cf8980fcfa
aasemgar7i <aasemgar7i@gamil.com>
2024-03-29 00:41:03 +0200

    +z

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
+"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)

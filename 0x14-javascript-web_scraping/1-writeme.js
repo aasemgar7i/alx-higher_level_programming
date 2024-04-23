3f9c20ba81980633d3a0400ec37a5684001234a0
aasemgar7i <aasemgar7i@gamil.com>
2024-04-23 20:29:20 +0200

    start2

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Check if both file path and string to write are provided
+if (process.argv.length !== 4) {
+  console.error('Usage: node 1-writeme.js <file_path> "<string_to_write>"');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+// Write the string to the file
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.error(err);
+  } else {
+    // Print a success message if writing was successful
+    console.log('Content has been written to the file:', filePath);
+  }
+});
